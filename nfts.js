217c8953a7a7ea1f0c6d50aed1a61e59bcb09409
ShivaniJaggi16 <137707056+ShivaniJaggi16@users.noreply.github.com>
2023-08-21 20:06:14 +0530

    Create nfts.js

---
+++ nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+    {
+        name: '0',
+        description: 'Alien versus a human',
+        image: 'QmSFePLz8cu9NKEMehnEhhFeQgowUYSYojPasjKU6cjMFq'
+    },
+    {
+        name: '1',
+        description: 'Alien versus a human',
+        image: 'QmcB4QK5XEDLrPBRmDnfHyfF1CdbgtrEw94tYzZzCZCHDJ'
+    },//change
+    {
+        name: '2',
+        description: 'Alien versus a human',
+        image: 'QmPnQ9VgaGLfBs9FVv5UhybPTTyMXcKKSCheFf9jLquGYf'
+    },
+    {
+        name: '3',
+        description: 'Alien versus a human',
+        image: 'QmSF1dV6fz7gjas6EbWDcEqtQV1TfjFsH5LE6Ex5ibwvzx'
+    },
+    {
+        name: '4',
+        description: 'Alien versus a human',
+        image: 'QmXuvSkRzHGZ3HZhYLtKcUuqqMjAAAC7nPWunb5SCezcJg'
+    },    
+];
+
+module.exports = nfts;
